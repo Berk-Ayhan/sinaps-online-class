91faea3ad67a4919d3834544c617a32f98043834
Berk Ayhan <berk.ayhan@globit.com.tr>
2024-07-07 20:46:41 +0300

    first commit

---
+++ src/test/java/com/sinaps/onlineclass/OnlineclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinaps.onlineclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
